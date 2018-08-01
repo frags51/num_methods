43fb540463b0697a8e0536ec3b486f069c3a3efb
Supreet Singh <supreet51.cs@gmail.com>
2018-08-01 14:45:58 +0530

    Fucking submodule bull shit

---
+++ hello.f95
@@ -0,0 +1,13 @@
+program blah
+implicit none
+
+    REAL :: a, b, res
+
+    a = 2
+    b = 3
+    res = a+b 
+
+    ! Prints useless spaces
+    print *,"Res is:",res
+
+end program blah
---
+++ type.f95
@@ -0,0 +1,9 @@
+program test
+implicit none
+
+    integer(kind=1) :: big
+    big = 24
+
+    print *, big
+
+end program test
